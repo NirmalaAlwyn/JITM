ba3ce6afca016399446df699c0b3856836705d60
jeevat0123 <jeevat0123@outlook.com>
2023-07-07 10:07:56 +0530

    downgrade 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,10 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { SigninComponent } from './signin/signin.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { PastvoicedropComponent } from './pastvoicedrop/pastvoicedrop.component';
-import { VdcallerinfoComponent } from './vdcallerinfo/vdcallerinfo.component';
-import { HeaderComponent } from './header/header.component';
-
-const routes: Routes = [
-  { path : '' , component:SigninComponent },
-  { path : 'login', component:SigninComponent },
-  { path : 'dashboard', component:DashboardComponent },
-  { path : 'pastvoicedrop', component:PastvoicedropComponent },
-  { path : 'vdcallerinfo', component:VdcallerinfoComponent} ,
-  { path : 'header', component:HeaderComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{onSameUrlNavigation: 'reload', useHash: false})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
